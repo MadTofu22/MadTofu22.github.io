c612f912a30c529e7bbf266d33f5bc314867b38f
MadTofu22 <stutler.tom@gmail.com>
2020-11-18 06:36:10 -0600

    formatted app.js as component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>App.js</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>App.js</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
