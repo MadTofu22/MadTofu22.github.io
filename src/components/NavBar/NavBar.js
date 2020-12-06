08bdb61c2538b45cb8cfea9f37d4e16b873ba47f
MadTofu22 <stutler.tom@gmail.com>
2020-12-05 19:28:07 -0600

    navbar and page formatting

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,16 +5,41 @@ import './Nav.css';
 // Import custom components
 import NavButton from './NavButton';
 
-
 class NavBar extends Component {
 
     render () {
+        const pages = [
+            {
+                path: '/home', 
+                label: 'Home'
+            },
+            {
+                path: '/about', 
+                label: 'About'
+            },
+            {
+                path: '/resume', 
+                label: 'Resume'
+            },
+            {
+                path: '/journey', 
+                label: 'Journey'
+            },
+            {
+                path: '/projects', 
+                label: 'Projects'
+            },
+            {
+                path: '/contact',
+                label: 'Contact'
+            },
+        ];
+
         return (
             <>
-                <NavButton path='/home' />
-                <NavButton path='/about' />
-                <NavButton path='/resume' />
-                <NavButton path='/journey' />
+                {pages.map(page => {
+                    return <NavButton page={page} />
+                })}
             </>
         );
     }
--- src/components/NavBar/NavButton.js
+++ src/components/NavBar/NavButton.js
@@ -3,6 +3,10 @@ import {withRouter} from 'react-router';
 
 class NavButton extends Component {
 
+    state = {
+        isClicked: false,
+    }
+
     // This function populates the path text for the button
     getBtnText = (path) => {
         switch (path) {
@@ -10,21 +14,33 @@ class NavButton extends Component {
                 return 'INVALID PATH';
             case '/home':
                 return 'Home';
-            case '/journey':
-                return 'My Journey'
-            case '/works':
-                return 'My Works'
             case '/about':
                 return 'About';
             case '/resume':
                 return 'Resume';
+            case '/journey':
+                return 'Journey'
+            case '/works':
+                return 'Works'
+            case '/contact':
+                return 'Contact'
         }
     }
 
     render () {
+        console.log(this.props)
         return (
-            <div className='navBtn' onClick={() => this.props.history.push(this.props.path)}>
-                <h2 className='navBtnLabel'>{this.getBtnText(this.props.path)}</h2>
+            <div 
+            className={
+                this.props.location.pathname === this.props.page.path ?
+                'navBtnClicked'
+                :
+                'navBtn'
+            } 
+            onClick={
+                () => this.props.history.push(this.props.page.path)
+            } >
+                <h2 className='navBtnLabel'>{this.props.page.label}</h2>
             </div>
         );
     }
