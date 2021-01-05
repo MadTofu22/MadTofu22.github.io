80fd822c8ef1650c6bd22b29555682da359b3a4c
MadTofu22 <stutler.tom@gmail.com>
2021-01-05 05:19:21 -0600

    modal display is better and contact has .env data now

--- src/components/ProjectsDisplay/ProjectModal.js
+++ src/components/ProjectsDisplay/ProjectModal.js
@@ -17,32 +17,40 @@ class ProjectDisplay extends Component {
                         onClick={this.props.closeProjectModal}
                         >Close
                     </button>
-                        <h1 className='modalHeader'>{this.state.projectData.name}</h1>
+                    <div className='modalHeader'>
+                        <h1 className='modalHeaderText'>{this.state.projectData.name}</h1>
+                    </div>
+                    <div className='modalContentSection'>
                         <img 
                         className='projectModalImage projects'
                         src={`assets/images/projects/${this.state.projectData.image}`}
                         alt={`Example image for ${this.state.projectData.name}`}
-                    />
-                    <h3>Description:</h3> 
-                    <p className='projectModalDescription'>{this.state.projectData.description}</p>
-                    <h3>Technologies Used:</h3>
-                    <ul className='projectModalList'>
-                        {this.state.projectData.technologies.map((item, index) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                    </ul>
-                    <button 
-                        onClick={
-                            () => window.open(this.state.projectData.repo, '_blank')
-                        }
-                        >View on Github
-                    </button>
-                    <button
-                        onClick={
-                            () => window.open(this.state.projectData.link, '_blank')
-                        }
-                        >Visit the App
-                    </button>
+                        />
+                        <div className='modalContentText'>
+                            <h3>Description:</h3>
+                            <p className='projectModalDescription'>{this.state.projectData.description}</p>
+                            <h3>Technologies Used:</h3>
+                            <ul className='projectModalList'>
+                                {this.state.projectData.technologies.map((item, index) => {
+                                    return <li key={index}>{item}</li>
+                                })}
+                            </ul>
+                            <div className='modalLinkButtons'>
+                                <button 
+                                    onClick={
+                                        () => window.open(this.state.projectData.repo, '_blank')
+                                    }
+                                    >View on Github
+                                </button>
+                                <button
+                                    onClick={
+                                        () => window.open(this.state.projectData.link, '_blank')
+                                    }
+                                    >Visit the App
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
